56a71210cbec77924ab67c78c9e757b494a2d37b
Yulia <y.varlakhina@gmail.com>
2022-11-15 20:52:39 +0300

    добавлен метод

--- less2 seminar/task09/Program.cs
+++ less2 seminar/task09/Program.cs
@@ -5,9 +5,22 @@
 //12-> 2
 //85 -> 8
 
-int number = new Random().Next(10,100);
+int number = new Random().Next(10, 100);
 Console.WriteLine($"случайное число из отрезка 10 - 99 --> {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-if(firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {firstDigit}");
-else Console.WriteLine($"наибольшая цифра числа {secondDigit}");
+
+//int firstDigit = number / 10;
+//int secondDigit = number % 10;
+//if (firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {firstDigit}");
+//else Console.WriteLine($"наибольшая цифра числа {secondDigit}");
+
+int MaxDigit(int num)
+{
+   int firstDigit = numb / 10;
+   int secondDigit = num % 10;
+   if (firstDigit > secondDigit) return firstDigit;
+   return secondDigit;
+}
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"наибольшая цифра числа {MaxDigit}");
+
+int maxDigit1 = MaxDigit(12);
