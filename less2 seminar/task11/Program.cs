c3e4498f6ed956d4db9447e20bbe14b3a986a5db
Yulia <y.varlakhina@gmail.com>
2022-11-15 21:14:41 +0300

    task11

---
+++ less2 seminar/task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// 456 -> 46
+int MinusMidlle(int num)
+{
+    int firstDigit = num / 100;
+    int secontDigit = num % 10;
+    int result = firstDigit * 10 + secontDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int number1 = MinusMidlle(number);
+
+Console.WriteLine(number1);
