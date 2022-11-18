57cb425533f6bfd8152cb0c0139e9db8766f486c
Yulia <y.varlakhina@gmail.com>
2022-11-18 23:15:53 +0300

    добавлена зада 22 и помощь по дз

--- less3 seminar/task18/Program.cs
+++ less3 seminar/task18/Program.cs
@@ -2,55 +2,32 @@
 //по заданному номеру четверти, показывает диапазон
 // возможных координат точек в этой четверти (x и y).
 
-//  Console.WriteLine ("Введите номер четверти");
-//  int number = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine ("Введите номер четверти");
+ int number = Convert.ToInt32(Console.ReadLine());
 
-//  string Range(int num)
-//  {
-//     if(num == 1) return "x > 0 и y > 0";
-//     if(num == 2) return "x < 0 и y > 0";
-//     if(num == 3) return "x < 0 и y < 0";
-//     if(num == 4) return "x > 0 и y < 0";
-//     return "неверный номер четверти";
-// }
-
-// string result = Range (number);
-// Console.WriteLine (result);
-
-
- Console.WriteLine("Введите номер четверти");
- string number = Console.ReadLine();
-
- string Range(string num)
+ string Range(int num)
  {
-    if(num == "1") return "x > 0 и y > 0";
-    if(num == "2") return "x < 0 и y > 0";
-    if(num == "3") return "x < 0 и y < 0";
-    if(num == "4") return "x > 0 и y < 0";
-    return "неверный номер четверти";
+    if(num == 1) return "x > 0 и y > 0";
+    if(num == 2) return "x < 0 и y > 0";
+    if(num == 3) return "x < 0 и y < 0";
+    if(num == 4) return "x > 0 и y < 0";
+    return "не верный номер четверти";
 }
-string result = Range(number);
+
+string result = Range (number);
 Console.WriteLine (result);
 
-//warning CS8600: Преобразование литерала, допускающего значение NULL 
-//или возможного значения NULL в тип, не допускающий значение NULL.
-  
-//warning CS8604: Возможно, аргумент-ссылка, допускающий значение NULL, 
-//для параметра "num" в "string Range(string num)". 
 
-// Console.WriteLine("введите номер четверти");
-// string number = Console.ReadLine();
+//  Console.WriteLine("Введите номер четверти");
+//  string number = Console.ReadLine();
 
-// string coord(string num)
-// {
-//     if (num == "1") return "x > 0, y > 0";
-//     if (num == "2") return "x < 0, y > 0";
-//     if (num == "3") return "x < 0, y < 0";
-//     if (num == "4") return "x > 0, y < 0";
-//     return "введены некорректные данные";
+//  string Range(string num)
+//  {
+//     if(num == "1") return "x > 0 и y > 0";
+//     if(num == "2") return "x < 0 и y > 0";
+//     if(num == "3") return "x < 0 и y < 0";
+//     if(num == "4") return "x > 0 и y < 0";
+//     return "не верный номер четверти";
 // }
-
-// string coordinates = coord(number);
-
-// Console.WriteLine(coordinates);
-
+// string result = Range(number);
+// Console.WriteLine (result);
