5e36f15d801b23254d815de470640eefca1f52f4
Yulia <y.varlakhina@gmail.com>
2022-11-22 22:01:22 +0300

    task30

--- less4 seminar/task30/Program.cs
+++ less4 seminar/task30/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int[] array = new int[8];
+
+// return array;
+
+// array [0] = 345
+// array [1] = 345
+// array [6] = 345
+
+// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        {
+         Console.Write($"{arr[i]},");
+        }
+}
+
+FillArray(array);
+PrintArray(array);
