4553d2d26c6db16c3b508cd99808fb1074fbbc82
Yulia <y.varlakhina@gmail.com>
2022-11-22 22:12:00 +0300

    дополнена task30

--- less4 seminar/task30/Program.cs
+++ less4 seminar/task30/Program.cs
@@ -11,11 +11,33 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+// int[] array = new int[8];
+//             void FillArray(int[] arr)
+//             {
+//                 Random rnd = new Random();
+//                 for (int i = 0; i < arr.Length; i++)
+//                 {
+//                     arr[i] = rnd.Next(0, 2);
+//                 }
+//             }
+//             void PrintArray(int[] arr)
+//             {
+//                 Console.Write("[");
+//                 for (int i = 0; i < arr.Length; i++)
+//                 {
+//                     Console.Write($"{arr[i]}, ");
+//                 }
+//                 Console.Write("]");
+//             }
+//             FillArray(array);
+//             PrintArray(array);
+
+
 int[] array = new int[8];
 
 void FillArray(int[] arr)
 {
-    for (int i = 0; i < arr.Length; i++)
+        for (int i = 0; i < arr.Length; i++)
     {
         array[i] = new Random().Next(0, 2);
     }
@@ -23,10 +45,12 @@ void FillArray(int[] arr)
 
 void PrintArray(int[] arr)
 {
+    Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
         {
          Console.Write($"{arr[i]},");
         }
+    Console.Write("]");
 }
 
 FillArray(array);
