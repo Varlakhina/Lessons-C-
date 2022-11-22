c8a4f112cf8e0fe309f86e25a007404ce4f67d7a
Yulia <y.varlakhina@gmail.com>
2022-11-22 21:05:33 +0300

    корректировка задачи26

--- less4 seminar/task26/Program.cs
+++ less4 seminar/task26/Program.cs
@@ -32,15 +32,14 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int NumberDigit(int num)
 {
-    //     for (int i = 0; i < num; i++)
     int digit = 0;
     while (num != 0)
     {
         num = num / 10;
         digit++;
     }
-    return digit;
+    return digit == 0 ? 1 : digit;
 }
 
 int digitNumber = NumberDigit(number);
-Console.WriteLine($"{number} -> {NumberDigit(number)}");
+Console.WriteLine($"{number} -> {digitNumber}");
